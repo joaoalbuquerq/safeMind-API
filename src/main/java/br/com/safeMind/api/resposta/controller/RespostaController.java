45fcec1dedd5e07845b241c1c1ce1df608947a0c
João Albuquerque <joao.albuquerque@gruporas.com.br>
2024-11-28 15:43:19 -0300

    Adicionando rotas CRUD da entidade RESPOSTA

--- src/main/java/br/com/safeMind/api/resposta/controller/RespostaController.java
+++ src/main/java/br/com/safeMind/api/resposta/controller/RespostaController.java
@@ -1,9 +1,47 @@
 package br.com.safeMind.api.resposta.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.safeMind.api.resposta.dto.DadosCadastroRespostaDTO;
+import br.com.safeMind.api.resposta.model.Resposta;
+import br.com.safeMind.api.resposta.service.RespostaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/respostas")
 public class RespostaController {
+
+    @Autowired
+    RespostaService respostaService;
+
+    @PostMapping
+    public ResponseEntity<Resposta>  cadastrar(@RequestBody @Valid DadosCadastroRespostaDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(respostaService.cadastrar(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Resposta>> listar(){
+        return ResponseEntity.ok(respostaService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Resposta> buscar(@PathVariable UUID id){
+        return ResponseEntity.ok(respostaService.buscarPorId(id))
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Resposta> atualizar(@PathVariable UUID id, @RequestBody @Valid DadosCadastroRespostaDTO dto){
+        return ResponseEntity.ok(respostaService.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable UUID id){
+        respostaService.remover(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/safeMind/api/resposta/service/RespostaService.java
+++ src/main/java/br/com/safeMind/api/resposta/service/RespostaService.java
@@ -1,7 +1,42 @@
 package br.com.safeMind.api.resposta.service;
 
+import br.com.safeMind.api.comon.exception.RecursoNaoEncontradoException;
+import br.com.safeMind.api.resposta.dto.DadosCadastroRespostaDTO;
+import br.com.safeMind.api.resposta.model.Resposta;
+import br.com.safeMind.api.resposta.repository.RespostaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RespostaService {
+
+    @Autowired
+    RespostaRepository respostaRepository;
+
+    public Resposta cadastrar(DadosCadastroRespostaDTO dto) {
+        Resposta resposta = new Resposta(dto);
+        return respostaRepository.save(resposta);
+    }
+
+    public List<Resposta> listar() {
+        return respostaRepository.findAll();
+    }
+
+    public Resposta buscarPorId(UUID id) {
+        return respostaRepository.findById(id).orElseThrow(() -> new RecursoNaoEncontradoException("Resposta inexistente"));
+    }
+
+    public Resposta atualizar(UUID id, DadosCadastroRespostaDTO dto) {
+        var resposta = buscarPorId(id);
+        resposta.setDescricao(dto.descricao());
+        return respostaRepository.save(resposta);
+    }
+
+    public void remover(UUID id) {
+        var resposta = buscarPorId(id);
+        respostaRepository.delete(resposta);
+    }
 }
